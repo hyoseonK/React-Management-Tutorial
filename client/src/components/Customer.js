be9621748f5b1e1172eeae403c2242a69884c6fe
hyoseonK <gytjs825@gmail.com>
2019-11-15 18:47:46 +0900

    Add Customer Delete Module

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CustomerDelete from './CustomerDelete';
 
 class Customer extends React.Component{ //한 명에 대한 정보를 출력해줌
     render(){ //항상 수행되는 내용
@@ -12,6 +13,7 @@ class Customer extends React.Component{ //한 명에 대한 정보를 출력해�
                 <TableCell>{this.props.birthday}</TableCell>
                 <TableCell>{this.props.gender}</TableCell>
                 <TableCell>{this.props.job}</TableCell>
+                <TableCell><CustomerDelete stateRefresh={this.props.stateRefresh} id={this.props.id}/></TableCell>
             </TableRow>
         )
     }
